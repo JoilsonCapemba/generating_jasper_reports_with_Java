b2103425cfdf3d878afe9e9b0e8a8de4ea38acf9
Joilson Capemba <joilsoncapemba@gmail.com>
2024-12-13 14:59:24 +0100

    first commit

---
+++ src/main/java/com/learnning/generating_jasper_reports_with_Java/GeneratingJasperReportsWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.learnning.generating_jasper_reports_with_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratingJasperReportsWithJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratingJasperReportsWithJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnning/generating_jasper_reports_with_Java/GeneratingJasperReportsWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnning.generating_jasper_reports_with_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratingJasperReportsWithJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
